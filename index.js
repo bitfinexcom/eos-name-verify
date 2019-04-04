82008f7f837820e041acdd94d2810b9810392cfb
Robert Kowalski <rok@kowalski.gd>
2019-04-04 19:21:42 +0200

    support auctioned eos domains

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = isValidAccountName
 
 function isValidAccountName (name) {
   if (typeof name !== 'string') return false
-  if (name.length !== 12) return false
+  if (name.length > 12) return false
   if (!/[a-z]/.test(name[0])) return false
 
-  return !/[^abcdefghijklmnopqrstuvwxyz12345\.]/.test(name)
+  return !/[^abcdefghijklmnopqrstuvwxyz12345.]/.test(name)
 }
