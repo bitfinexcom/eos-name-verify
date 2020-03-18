d7c0c1e67df4be4399fa8d40555150667ef0835a
Leask Wong <i@leaskh.com>
2020-03-18 14:01:18 -0400

    [45.357503,-75.634252] updated regexp

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = isValidAccountName
 
-function isValidAccountName (name) {
-  return typeof name === 'string' 
-      && /(^[a-z1-5\.]{1,11}[a-z1-5]$)|(^[a-z1-5\.]{12}[a-j1-5]$)/.test(name);
+function isValidAccountName(name) {
+  return typeof name === 'string'
+    && /(^[a-z1-5.]{0,11}[a-z1-5]$)|(^[a-z1-5.]{12}[a-j1-5]$)/.test(name);
 }
